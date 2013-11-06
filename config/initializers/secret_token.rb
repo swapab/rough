f336c26d25b699d0a93088ff207ac81a5a00b76d
Swapnil Abnave <swapnil.abnave@kiprosh.com>
2013-11-06 17:01:14 +0530

    Lets roll.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rough::Application.config.secret_token = '278ca23f90e11bf047d2acc492910bd827d9974efcffbd2ba1c66223bbeeb1426cffe8b8254c54276f9d5278969b5a343b14df71a6c1a0990cea13beb939c47c'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rough::Application.routes.draw do
+  get "home/index"
+  root to: 'home#index'
+end
